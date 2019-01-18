0532658825999c6f394c9d14680268b54b3cc765
husainalamsyah <Husain4life@gmail.com>
2019-01-18 23:56:04 +0700

    create mastering react app

---
+++ src/components/project.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+
+        return  (
+            <React.Fragment>
+            <h1>Hello earth</h1>
+            <button>Increase</button>
+            </React.Fragment>
+        
+        );
+    }
+}
+ 
+export default Project;
