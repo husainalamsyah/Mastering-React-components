7a51f1dc33d38b148870eca706f4c4d4999370f6
husainalamsyah <43976170+husainalamsyah@users.noreply.github.com>
2019-01-24 21:31:50 +0700

    Rendering Classes

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 
 class Project extends Component {
     state = {
-        angka: 3
+        angka: 1
     };
     
     render() {
-
         return  (
             <div>
-            <span style={{fontSize: 20}} className="badge badge-danger m-2">{this.formatAngka()}</span>
+            <span style={{fontSize: 20}} className={this.getBadgeKelas()}> {this.formatAngka()}</span>
             <button className="btn btn-primary btn-sm">Increase</button>
             </div>
         
         );
+    }
+
+    getBadgeKelas() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.angka === 3 ? "danger" : "warning";
+        return classes;
     }
 
     formatAngka(){
