8eaa9284fd4613336f0a0550da8eca3ce600d500
husainalamsyah <43976170+husainalamsyah@users.noreply.github.com>
2019-01-25 21:30:19 +0700

    Conditional Rendering

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,32 +3,38 @@ import React, { Component } from 'react';
 class Project extends Component {
     state = {
         angka: 3,
-        tags: ['tag1','tag2','tag3']
+        tags: []
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>tidak ada tags disini</p>
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
     
     render() {
         return  (
             <div>
-            <span style={{fontSize: 20}} className={this.getBadgeKelas()}> {this.formatAngka()}</span>
-            <button className="btn btn-primary btn-sm">Increase</button>
-            <ul>
-                { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
-            </ul>
+            {/* <span style={{fontSize: 20}} className={this.getBadgeKelas()}> {this.formatAngka()}</span>
+            <button className="btn btn-primary btn-sm">Increase</button> */}
+            {this.state.tags.length === 0 && "buat tag baru!"}
+            { this.renderTags() }
+            
             </div>
         
         );
     }
 
-    getBadgeKelas() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.angka === 3 ? "danger" : "warning";
-        return classes;
-    }
+    // getBadgeKelas() {
+    //     let classes = "badge m-2 badge-";
+    //     classes += this.state.angka === 3 ? "danger" : "warning";
+    //     return classes;
+    // }
 
-    formatAngka(){
-        const {angka} = this.state;
-        return angka === 3 ? 'Three' : angka;
-    }
+    // formatAngka(){
+    //     const {angka} = this.state;
+    //     return angka === 3 ? 'Three' : angka;
+    // }
 }
  
 export default Project;
