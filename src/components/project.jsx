23950466dfc664033c4d604c132cf186ddaf10d5
husainalamsyah <43976170+husainalamsyah@users.noreply.github.com>
2019-01-26 20:21:49 +0700

    Handling Events

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,35 +6,44 @@ class Project extends Component {
         tags: []
     };
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>tidak ada tags disini</p>
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>tidak ada tags disini</p>
 
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    // }
+
+    handleIncrease(){
+        console.log("Increase Clicked", this);
     }
     
     render() {
         return  (
             <div>
-            {/* <span style={{fontSize: 20}} className={this.getBadgeKelas()}> {this.formatAngka()}</span>
-            <button className="btn btn-primary btn-sm">Increase</button> */}
-            {this.state.tags.length === 0 && "buat tag baru!"}
+            <span style={{fontSize: 20}} className={this.getBadgeKelas()}> {this.formatAngka()}</span>
+            <button 
+            onClick={this.handleIncrease}
+            className="btn btn-primary btn-sm"
+            >
+            Increase
+            </button>
+            {/* {this.state.tags.length === 0 && "buat tag baru!"}
             { this.renderTags() }
-            
+             */}
             </div>
         
         );
     }
 
-    // getBadgeKelas() {
-    //     let classes = "badge m-2 badge-";
-    //     classes += this.state.angka === 3 ? "danger" : "warning";
-    //     return classes;
-    // }
+    getBadgeKelas() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.angka === 3 ? "danger" : "warning";
+        return classes;
+    }
 
-    // formatAngka(){
-    //     const {angka} = this.state;
-    //     return angka === 3 ? 'Three' : angka;
-    // }
+    formatAngka(){
+        const {angka} = this.state;
+        return angka === 3 ? 'Three' : angka;
+    }
 }
  
 export default Project;
